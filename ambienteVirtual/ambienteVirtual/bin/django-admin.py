99dc040fb7a14a009d47c969eb6916a362457574
Gian <gian.bravo@alumnos.usm.cl>
2018-04-21 19:06:59 -0300

    se agrega ambiente virtual

---
+++ ambienteVirtual/ambienteVirtual/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/leo/Escritorio/Prubit/ambienteVirtual/ambienteVirtual/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
