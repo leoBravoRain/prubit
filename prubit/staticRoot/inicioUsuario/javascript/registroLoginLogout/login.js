2d9dbe876737468fbc39fa918216ca73c2d5baad
Gian <gian.bravo@alumnos.usm.cl>
2018-05-09 18:36:21 -0300

    se vuelve a agregar boton de login con FB

--- prubit/staticRoot/inicioUsuario/javascript/registroLoginLogout/login.js
+++ prubit/staticRoot/inicioUsuario/javascript/registroLoginLogout/login.js
@@ -31,8 +31,12 @@ $(document).ready(function() {
 // Funcion para loguear a usuario en Prubit
 function loginAPrubit() {
 
+	console.log("Se ejecuta funcion loginAPrubit()");
+
 	// Se utiliza API de Facebook para obtener datos y enviarlos a backend
   	FB.api('/me',{fields: 'email' }, function(response){
+
+  		console.log("se obtienen los datos del usuario desde FB");
 
   		// Expresion regular para verificar email
   		var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
@@ -53,11 +57,15 @@ function loginAPrubit() {
 
 		  		success: function(responseAJAX){
 
+		  			console.log("Se obtienen respuesta desde server");
+
+		  			console.log(responseAJAX);
+		  			
 		  			// Si fue todo correcto
 		  			if(responseAJAX.usuarioExiste && responseAJAX.logueado){
 
 		  				// Mensaje de exito de login
-		  				console.log("success login with FB");
+		  				console.log("succeddlogin with FB");
 
 		  				// Se redirige hacia el index
 						location.assign(urlIndex);
@@ -96,11 +104,19 @@ function loginAPrubit() {
 // Funcion ejecutada al apretar el boton de login de facebook (en template)
 function checkLoginState() {
 
+	console.log("click en boton login");
+
 	// Se chequea estado de login de usuario en app de Prubit de Facebook
 	FB.getLoginStatus(function(response) {
 
+		console.log("se chequea estado de usuario en app de Prubit en FB");
+
+		consol.log(response);
+
 	    // Si usuario esta logeado en Facebook y en mi app de Facebook
 		if (response.status === 'connected') {
+
+			console.log("usuario esta conectado");
 
 			// Se ejecuta funcion para loguear en Prubit
 			loginAPrubit();
