04f86f2761dd0ce1d05bbe640e6fd1c5df487bb6
Gian <gian.bravo@alumnos.usm.cl>
2018-05-09 18:30:02 -0300

    se vuelve a agregar boton de login con FB

--- prubit/staticRoot/inicioUsuario/javascript/registroLoginLogout/login.js
+++ prubit/staticRoot/inicioUsuario/javascript/registroLoginLogout/login.js
@@ -56,6 +56,9 @@ function loginAPrubit() {
 		  			// Si fue todo correcto
 		  			if(responseAJAX.usuarioExiste && responseAJAX.logueado){
 
+		  				// Mensaje de exito de login
+		  				console.log("success login with FB");
+
 		  				// Se redirige hacia el index
 						location.assign(urlIndex);
 
