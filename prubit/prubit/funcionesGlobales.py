a5148ae2d5017b827bcc3a000ff920a1fdef3672
Gian <gian.bravo@alumnos.usm.cl>
2018-05-10 13:59:30 -0300

    se agrega boton login de FB, se agrega metodo para rotar imagenes de celular que vienen rotadas, se agrega gender en formulario de registro por SM

--- prubit/prubit/funcionesGlobales.py
+++ prubit/prubit/funcionesGlobales.py
@@ -130,43 +130,75 @@ def resizePhoto(self, maxWidth, maxHeight, Garment, GarmentsToCheck):
 # 		            print "se almacena la imagen"
 # 		            image.save(foto.path, file_format)
 
+# def imageAutorotate(foto):
+
+# 	with Image.open(foto) as image:
+
+# 		print image.show()
+
+# 		print "Se ingresa a with "
+
+# 		file_format = image.format
+
+# 		print file_format
+
+# 		# Se chequea si es JPEG, ya que solo se puede extraer el exif solo desde JPEG (Usando PIL)
+# 		if file_format == "JPEG":
+
+# 			exif = image._getexif()
+
+# 			print exif
+
+# 			# image.thumbnail((1667, 1250), resample=Image.ANTIALIAS)
+
+# 			# if image has exif data about orientation, let's rotate it
+# 			orientation_key = 274 # cf ExifTags
+
+# 			if exif and orientation_key in exif:
+
+# 				orientation = exif[orientation_key]
+
+# 				print orientation
+
+# 				rotate_values = {
+# 				3: Image.ROTATE_180,
+# 				6: Image.ROTATE_270,
+# 				8: Image.ROTATE_90
+# 				}
+
+# 				if orientation in rotate_values:
+
+# 					image = image.transpose(rotate_values[orientation])
+# 					print "orientacion esta dentro de rotate_values"
+# 					print "se almacena la imagen"
+# 					image.save(foto.path, file_format)
+
+
+# Funcion para rotar imagens si provienen de iOs (ya que por defecto se rotan en PC, pero en cellphones no aparecen rotadas)
 def imageAutorotate(foto):
 
-	with Image.open(foto) as image:
+    with Image.open(foto) as image:
+        file_format = image.format
 
-		print "Se ingresa a with "
+        # Se chequea si es JPEG, ya que solo se puede extraer el exif solo desde JPEG (Usando PIL)
+        if file_format == "JPEG":
 
-		file_format = image.format
+            exif = image._getexif()
 
-		print file_format
+            # image.thumbnail((1667, 1250), resample=Image.ANTIALIAS)
 
-		# Se chequea si es JPEG, ya que solo se puede extraer el exif solo desde JPEG (Usando PIL)
-		if file_format == "JPEG":
+            # if image has exif data about orientation, let's rotate it
+            orientation_key = 274 # cf ExifTags
+            if exif and orientation_key in exif:
+                orientation = exif[orientation_key]
 
-			exif = image._getexif()
+                rotate_values = {
+                    3: Image.ROTATE_180,
+                    6: Image.ROTATE_270,
+                    8: Image.ROTATE_90
+                }
 
-			print exif
+                if orientation in rotate_values:
+                    image = image.transpose(rotate_values[orientation])
 
-			# image.thumbnail((1667, 1250), resample=Image.ANTIALIAS)
-
-			# if image has exif data about orientation, let's rotate it
-			orientation_key = 274 # cf ExifTags
-
-			if exif and orientation_key in exif:
-
-				orientation = exif[orientation_key]
-
-				print orientation
-
-				rotate_values = {
-				3: Image.ROTATE_180,
-				6: Image.ROTATE_270,
-				8: Image.ROTATE_90
-				}
-
-				if orientation in rotate_values:
-
-					image = image.transpose(rotate_values[orientation])
-					print "orientacion esta dentro de rotate_values"
-					print "se almacena la imagen"
-					image.save(foto.path, file_format)
+            image.save(foto.path, file_format)
